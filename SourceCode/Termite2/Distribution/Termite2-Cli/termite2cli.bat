c24e0930b80d29789d27572d004b7c02f70f3ad5
Fernando Moreira <fernandomoreira1996@gmail.com>
2020-12-29 17:15:00 +0000

    Small error
    line 10 "...\Termite-Cli.jar" to "...\Termite2-Cli.jar"

--- SourceCode/Termite2/Distribution/Termite2-Cli/termite2cli.bat
+++ SourceCode/Termite2/Distribution/Termite2-Cli/termite2cli.bat
@@ -7,7 +7,7 @@ if "%TERMITE2_CLI_PATH%"=="" (
 
 set jline=%TERMITE2_CLI_PATH%\libs\jline-2.13.jar
 set commonios=%TERMITE2_CLI_PATH%\libs\commons-io-2.6.jar
-set termite2=%TERMITE2_CLI_PATH%\libs\Termite-Cli.jar
+set termite2=%TERMITE2_CLI_PATH%\libs\Termite2-Cli.jar
 set deps="%jline%;%commonios%;%termite2%"
 java -cp %deps% main.pt.inesc.termite2.cli.Main %*
 
